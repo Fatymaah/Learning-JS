17d8b486f57f7c53cea76c94336e2abd91441a53
Fatymaah <35401881+Fatymaah@users.noreply.github.com>
2018-03-10 11:36:19 +0300

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+var student = ["Jane","Mary","Lauren","Ivy","Fatma","Judith","Irene"];
+var shoeSize =[40,34,42,44,38,36,41];
+// Get length of aarray
+//console.log(student.length)
+for (i =0; i<=student.length-1; ++i) {
+	console.log(student[i]);
+
+} 
+
+
