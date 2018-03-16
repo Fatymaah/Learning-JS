dcf455c92c4b42cbea050f6e337a4b1b2acfb149
Fatymaah <35401881+Fatymaah@users.noreply.github.com>
2018-03-16 15:30:35 +0300

    Add files via upload

---
+++ FunctionParameters.js
@@ -0,0 +1,21 @@
+function areaOfTriangle(height,base){
+	
+	var answer=0.5*base*height;
+
+	return answer;
+
+}
+
+ var result=areaOfTriangle(5,4)
+ var a =areaOfTriangle(7,9)
+ var b =areaOfTriangle(3,2)
+ // console.log("Triangle One" + result)
+ // console.log(, "Triangle Two" + a)
+ // console.log("Triangle Three" + b)
+
+ print(result)
+ print(a)
+
+ function print(result2){
+ 	console.log("Area of Triangle" + result2)
+ }
